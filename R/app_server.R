579ac8a4180386dcf9bbb9d90ec910b34a2e6d6f
cristinamullin <mullin.cristina@epa.gov>
2022-12-19 14:03:45 -0500

    data upload
    Added data upload button and connected it to the import module

--- R/app_server.R
+++ R/app_server.R
@@ -4,8 +4,14 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
+
+# Below increases the max data upload size from the Shiny default of 5MB per file
+# to 30MB for file
+options(shiny.maxRequestSize=30*1024^2)
+
 app_server <- function( input, output, session ) {
   # Your application server logic 
+  
   TADA_Profile <- reactiveVal(NULL)
   
   modImportTADAProfile <- mod_upload_TADAprofile_server("upload_TADAprofile_1", TADA_Profile)
