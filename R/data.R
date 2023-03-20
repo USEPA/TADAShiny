9e1fe406fb9cf1d316cf901083d81720ddf8e50c
Mullin <Mullin.Cristina@epa.gov>
2023-03-20 12:07:42 -0400

    render summary table

--- R/data.R
+++ R/data.R
@@ -1,21 +1,3 @@
-#' Lake Water Temperature
-#'
-#' TADA example dataset containing all lake water temperature data in the U.S.
-#' from Jan. 2000 to July 2022. This dataset can be reproduced 
-#' with the following TADABigdataRetrieval call: 
-#' 
-#' AllWaterTempData <- TADABigdataRetrieval(startDate = "2000-01-01", 
-#' endDate = "2022-07-01", 
-#' characteristicName = "Temperature, water",
-#' siteType = "Lake, Reservoir, Impoundment")
-#'
-#' @docType data
-#' @keywords datasets
-#' @name WaterTemp_US
-#' @usage data(WaterTemp_US)
-#' @format A data frame with 14237 rows and 103 variables
-NULL
-
 #' Utah Nutrient Data
 #'
 #' A dataset containing Ammonia, Nitrate, and Nitrogen data from Utah:
