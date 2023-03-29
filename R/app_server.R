1d7a170e87d303fc34404a1028bfd525c2a4624e
Elise H <121896266+ehinman@users.noreply.github.com>
2023-03-29 19:39:20 -0400

    combine first module to pull data, add example data.

--- R/app_server.R
+++ R/app_server.R
@@ -13,8 +13,17 @@ app_server <- function(input, output, session) {
   # Your application server logic
   tadat = reactiveValues() # create a list object that holds reactive values passed between modules
   mod_query_data_server("query_data_1", tadat) # server call to the module servers with the name of the module and any dependecies (this one uses the tadat reactive values object)
-  mod_upload_data_server("upload_data_1", tadat)
   mod_data_flagging_server("data_flagging_1", tadat)
   mod_summary_server("summary_1", tadat)
   mod_overview_server("overview_1", tadat)
+  
+  observeEvent(tadat$overview==1, {
+    appendTab(inputId="tabbar",tab=tabPanel("Overview",
+                                                     br(),
+                                                     mod_overview_ui("overview_1"),
+                                                     hr(),
+                                                     mod_summary_ui("summary_1"),
+                                                     hr(),
+                                                     mod_TADA_summary_ui("TADA_summary_1")))
+  })
 }
