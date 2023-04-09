e750fce7651223d8642b7890ad53c2000835ec78
ElinorK <16242091+elinork@users.noreply.github.com>
2023-04-09 09:30:32 -0400

    back to download_packages from before

--- scripts/download_packages.R
+++ scripts/download_packages.R
@@ -2,10 +2,19 @@ library(stringr)
 
 args <- commandArgs(trailingOnly = TRUE)
 
+options(repos = 
+        "https://packagemanager.rstudio.com/cran/__linux__/jammy/latest")
+
 # Test if a package destination directory was passed as an argument
 if (length(args) == 0) {
         stop("Usage: R -f download_packages.R destination_directory 
         package1,package2,package3", call. = FALSE)
+}
+
+# Create the directory
+if (!dir.exists(args[1])) {
+        message("Creating the directory")
+        dir.create(args[1], recursive = TRUE)
 }
 
 # Split the packages into a character list
@@ -13,21 +22,104 @@ input_packs <- unlist(str_split(args[2], ","))
 
 message(paste(input_packs), sep = " ")
 
-deps <- pak::pkg_deps(input_packs)
+plfrm <- paste(getRversion(), R.version["platform"],
+        R.version["arch"], R.version["os"])
 
-for (row in 1:nrow(deps)) {
-        dl <- pak::pkg_download(deps[row, "ref"], dest_dir = args[1],
-                dependencies = FALSE)
-        if ("fulltarget_tree" %in% names(dl)) {
-                # Indicates package needs to be built
-                print(dl$fulltarget_tree)
-                dir.create("junktemp")
-                unzip(dl$fulltarget_tree, exdir = "junktemp")
-                f <- list.files(path = "junktemp")
-                print(f)
-                devtools::build(pkg = paste("junktemp", f[1], sep = "/"),
-                        path = args[1], binary = TRUE)
-                unlink("junktemp", recursive = TRUE)
-                file.remove(dl$fulltarget_tree)
-        }
+options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), plfrm))
+
+# Get packages function to get the packages and dependencies
+get_packages <- function(packs) {
+        message("Getting the package dependencies")
+        packages <- unlist(
+                tools::package_dependencies(packs, available.packages(),
+                        which = c("Depends", "Imports"), recursive = TRUE
+                )
+        )
+        packages <- union(packs, packages)
+        packages
 }
+
+# Install any GitHub packages which are not available in
+# Posit Public Package Manager but some of their dependencies may be
+github_packages_known_list <- list(c("TADA", "USEPA/TADA"))
+gh_pkg_names <- sapply(github_packages_known_list, "[[", 1)
+github_packages_to_build <- list()
+for (p in intersect(input_packs, gh_pkg_names)) {
+        gh_pkg <- github_packages_known_list[gh_pkg_names == p]
+        message(paste("Github package", p, "found", sep = " "))
+        # Install package to also install dependencies:
+        pak::pkg_install(gh_pkg[[1]][2])
+        deps <- pak::pkg_deps(gh_pkg[[1]][2])
+        in_cran <- deps["ref"][deps["type"] == "standard"]
+        gpk <- unlist(deps[deps["type"] == "github", c("package", "ref")])
+        i <- 1
+        while (i < length(gpk)) {
+                github_packages_to_build <- rlist::list.append(
+                        github_packages_to_build, c(gpk[i], gpk[i + 1]))
+                i <- i + 2
+        }
+        input_packs <- union(input_packs, in_cran)
+}
+
+message(paste("Github packages needed are:",
+        github_packages_to_build, sep = " "))
+
+# Get the package dependencies from Posit Public Package Manager
+packages <- get_packages(setdiff(input_packs, gh_pkg_names))
+
+download_and_build_cran_package <- function(pack) {
+        dir.create("junktemp")
+        download.packages(c(pack), destdir = "junktemp")
+        pkg <- list.files("junktemp")[1]
+        devtools::build(pkg = paste("junktemp", pkg, sep = "/"),
+                        path = args[1], binary = TRUE)
+        unlink("junktemp", recursive = TRUE)
+}
+
+packages_needing_to_be_built <- c("sf")
+
+# Download the packages from the repository
+message(paste("Downloading the packages and dependencies to",
+                args[1], sep = " "))
+download.packages(setdiff(packages, packages_needing_to_be_built),
+                destdir = args[1])
+
+for (p in intersect(packages, packages_needing_to_be_built)) {
+        message(paste("Package", p, "requires special handling", sep = " "))
+        download_and_build_cran_package(p)
+}
+
+message("Completed downloading packages")
+
+download_github_package <- function(pack) {
+        dir.create("junktemp")
+        dl <- pak::pkg_download(pack[2], dest_dir = "junktemp",
+                dependencies = FALSE,
+                platforms = "source")
+        if ("fulltarget_tree" %in% names(dl)) {
+                print(dl$fulltarget_tree)
+                dir.create("junktemp2")
+                unzip(dl$fulltarget_tree, exdir = "junktemp2")
+                f <- list.files(path = "junktemp2")
+                print(f)
+                devtools::build(pkg = paste("junktemp2", f[1], sep = "/"),
+                        path = args[1], binary = TRUE)
+                unlink("junktemp2", recursive = TRUE)
+        } else {
+                file.copy(dl$fulltarget, args[1])
+        }
+        unlink("junktemp", recursive = TRUE)
+}
+
+for (p in github_packages_to_build) {
+        message(paste("Package", p[1], "must be built from GitHub", sep = " "))
+        download_github_package(p)
+}
+
+tools::write_PACKAGES(dir = args[1], fields = NULL,
+        type = c("source"),
+        verbose = FALSE, unpacked = FALSE, subdirs = FALSE,
+        latestOnly = TRUE, addFiles = FALSE, rds_compress = "xz",
+        validate = FALSE)
+
+message("Wrote package description files")
