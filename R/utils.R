6d1874112c070a86632073a1aaafa9c0b83cf289
cristinamullin <mullin.cristina@epa.gov>
2023-06-05 20:55:23 -0400

    add utilities

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+utils::globalVariables(c("TADA.CensoredData.Flag", "ResultIdentifier", 
+                         "num", "Label", "TADA.ResultMeasureValue", 
+                         "TADA.CensoredMethod", "TADA.Remove", 
+                         "MonitoringLocationIdentifier", "MonitoringLocationName", 
+                         "TADA.LatitudeMeasure", "TADA.LongitudeMeasure", 
+                         "ActivityStartDate", "TADA.CharacteristicName", 
+                         "OrganizationIdentifier", "OrganizationFormalName", 
+                         "Result_Count", "TADA.Chars"))
