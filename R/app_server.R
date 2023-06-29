308d80c722d3b7e636a74cf3c3ea63b940d5ae94
Elise H <121896266+ehinman@users.noreply.github.com>
2023-06-29 09:05:34 -0400

    Update app_server.R
    added disclaimer upon app start

--- R/app_server.R
+++ R/app_server.R
@@ -20,12 +20,17 @@ app_server <- function(input, output, session) {
   mod_censored_data_server("censored_data_1", tadat)
   mod_review_data_server("review_data_1", tadat)
   mod_TADA_summary_server("TADA_summary_1", tadat)
-
+  
+  # this ensures all tabs except Upload are disabled upon app start
   shinyjs::disable(selector = '.nav li a[data-value="Overview"]')
   shinyjs::disable(selector = '.nav li a[data-value="Flag"]')
   shinyjs::disable(selector = '.nav li a[data-value="Filter"]')
   shinyjs::disable(selector = '.nav li a[data-value="Censored"]')
   shinyjs::disable(selector = '.nav li a[data-value="Review"]')
+  
+  shiny::showModal(shiny::modalDialog(title = "Disclaimer",
+                                      "This United States Environmental Protection Agency (EPA) GitHub project code is provided on an 'as is' basis and the user assumes responsibility for its use. EPA has relinquished control of the information and no longer has responsibility to protect the integrity, confidentiality, or availability of the information. Any reference to specific commercial products, processes, or services by service mark, trademark, manufacturer, or otherwise, does not constitute or imply their endorsement, recommendation or favoring by EPA. The EPA seal and logo shall not be used in any manner to imply endorsement of any commercial product or activity by EPA or the United States Government."))
+  
   # switch to overview tab when tadat$new changes and provide user with window letting them know how many records were automatically flagged for removal upon upload
   # move this to query_data?
   shiny::observeEvent(tadat$new,{
