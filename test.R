c7bde9b72df168d7502628ff78db7669e8a13a03
elinork <16242091+elinork@users.noreply.github.com>
2023-09-12 13:37:24 -0400

    Delete test.R

--- test.R
+++
@@ -1,8 +0,0 @@
-library(stringr)
-library(rjson)
-library(dplyr)
-
-input_packs <- "TADA,config,golem,magrittr,htmltools,readxl,writexl,shiny,leaflet,shinyWidgets,shinycssloaders,DT,ggplot2,shinybusy,dplyr,plyr,scales,forcats,testthat,remotes,covr,rmarkdown,knitr,spelling,sf,shinyjs,stringr,shinyjqui"
-
-refs <- strsplit(input_packs, split = ",")[[1]]
-
