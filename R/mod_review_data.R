760fbed697c557b48f1513f8d2b7040b76287b13
cristinamullin <46969696+cristinamullin@users.noreply.github.com>
2025-05-28 13:51:54 -0400

    add stringr and reference EPATADA develop branch

--- R/mod_review_data.R
+++ R/mod_review_data.R
@@ -139,7 +139,7 @@ mod_review_data_server <- function(id, tadat) {
       }
       dat <- dat %>%
         dplyr::rowwise() %>%
-        dplyr::mutate(Legend = EPATADA::TADA_InsertBreaks(Legend, len = 100))
+        dplyr::mutate(Legend = stringr::str_wrap(Legend))
 
       # define number of colors required for pie chart
       colorCount <- length(unique(dat$Legend))
