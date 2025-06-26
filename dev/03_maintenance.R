4ad5161218ab68a5f34b424d122cfaff68d78405
cristinamullin <46969696+cristinamullin@users.noreply.github.com>
2025-06-26 15:25:46 -0400

    small check issues

--- dev/03_maintenance.R
+++ dev/03_maintenance.R
@@ -18,12 +18,30 @@ renv::status()
 # renv::restore(), then renv::install(), then renv::snapshot(), 
 # but that also suggests you should be running status more frequently.
 
+# spell check
+library(spelling)
+spelling::spell_check_package(
+  pkg = ".",
+  vignettes = TRUE
+)
+# run to update spelling word list
+spelling::get_wordlist()
+spelling::update_wordlist()
+
+# Run styler to style code
+# https://style.tidyverse.org/
+# See: https://styler.r-lib.org/reference/style_pkg.html
+# Run the following with defaults
+library(styler)
+style_pkg()
+
 ## Run checks ----
 ## Check the package before sending to prod
 ## Run devtools test, check and build
 devtools::document() # make sure all dependencies are listed in description file
 devtools::test()
-devtools::check()
+# more robust test for releases (includes broken link check)
+devtools::check(manual = FALSE, remote = TRUE, incoming = TRUE)
 # devtools::build()
 
 # # You're now set! ----
