c8caab4b13c5117917a84b32db74e368c5182e95
cristinamullin <46969696+cristinamullin@users.noreply.github.com>
2025-07-10 14:46:52 -0400

    update tests

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -18,40 +18,41 @@ test_that("app server", {
   }
 })
 
-# test_that(
-#   "app_sys works",
-#   {
-#     expect_true(
-#       golem::app_sys("golem-config.yml") != ""
-#     )
-#   }
-# )
+test_that(
+  "app_sys works",
+  {
+    expect_true(
+      app_sys("golem-config.yml") != ""
+    )
+  }
+)
 
-# test_that(
-#   "golem-config works",
-#   {
-#     config_file <- app_sys("golem-config.yml")
-#     skip_if(config_file == "")
-# 
-#     expect_true(
-#       get_golem_config(
-#         "app_prod",
-#         config = "production",
-#         file = config_file
-#       )
-#     )
-#     expect_false(
-#       get_golem_config(
-#         "app_prod",
-#         config = "dev",
-#         file = config_file
-#       )
-#     )
-#   }
-# )
+test_that(
+  "golem-config works",
+  {
+    config_file <- app_sys("golem-config.yml")
+    skip_if(config_file == "")
+
+    expect_true(
+      get_golem_config(
+        "app_prod",
+        config = "production",
+        file = config_file
+      )
+    )
+    expect_false(
+      get_golem_config(
+        "app_prod",
+        config = "dev",
+        file = config_file
+      )
+    )
+  }
+)
 
 # # Configure this test to fit your need.
-# # testServer() function makes it possible to test code in server functions and modules, without needing to run the full Shiny application
+# # testServer() function makes it possible to test code in server functions 
+# # and modules, without needing to run the full Shiny application
 # testServer(app_server, {
 # 
 #   # Set and test an input
