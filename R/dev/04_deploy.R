ccde05fedfcd682ba2bdd628f4d6192be79b9ab8
Mullin <Mullin.Cristina@epa.gov>
2025-08-12 16:50:49 -0400

    add project file and remove renv

--- R/dev/04_deploy.R
+++ R/dev/04_deploy.R
@@ -37,16 +37,6 @@ rsconnect::deployApp(
 
 # This is how to deploy to EPA's Posit Connect
 # golem::add_positconnect_file() # already exists
-rsconnect::deployApp(appDir = getwd(),
-                     account = "Cristina", 
-                     appFiles = c("app.R", "DESCRIPTION", "NAMESPACE", "R/", "inst/"),
-                     quarto = FALSE,
-                     server = "rstudio-connect.dmap-stage.aws.epa.gov",      
-                     appName = "TADAShiny", 
-                     appTitle = "TADAShiny",
-                     appId = 403, 
-                     launch.browser = TRUE, 
-                     lint = TRUE, 
-                     metadata = list(asMultiple = FALSE, asStatic = FALSE),      
-                     logLevel = "verbose", 
-                     forceUpdate = TRUE)
+# run these (update path)
+options(rsconnect.packrat = TRUE)
+rsconnect::deployApp("path/to/the/app")
