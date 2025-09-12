cfe7e4bcb344417a56c255c44774bde8cf4da54a
Mullin <Mullin.Cristina@epa.gov>
2025-09-12 17:11:27 -0400

    update WQP query filter values

--- R/mod_query_data.R
+++ R/mod_query_data.R
@@ -10,6 +10,9 @@
 data_path1 <- app_sys("extdata/statecodes_df.Rdata")
 load(data_path1)
 
+# This only includes the monitoring location IDs now (mlids)
+# That one takes too long to run on the fly
+# See 03_maintenance.R to update query_choices.Rdata periodically
 data_path2 <- app_sys("extdata/query_choices.Rdata")
 load(data_path2)
 
@@ -38,26 +41,52 @@ return_tribal_sf <- function(tribal_layer, tribal_name, tribal_list = tribal_lis
   return(tribal_data2)
 }
 
-# new (2024-05-23) list for new Country/Ocean(s) Query the Water Quality Portal option. Not included in saved query_choices file
+# Country/Ocean(s) query options the Water Quality Portal
+# Define the URL
 countrycode_url <- "https://www.waterqualitydata.us/Codes/countrycode?mimeType=json"
-countryocean_source <- jsonlite::fromJSON(txt = countrycode_url)
-countryocean_source <- countryocean_source$codes %>% dplyr::select(-one_of("providers"))
-countryocean_source <- countryocean_source[order(countryocean_source$desc), ]
-countryocean_choices <- countryocean_source$value
-names(countryocean_choices) <- countryocean_source$desc
+# Fetch and parse JSON data using httr
+response <- httr::GET(countrycode_url)
+httr::stop_for_status(response)  # Check for request errors
+countryocean_source <- httr::content(response, as = "text", encoding = "UTF-8")
+countryocean_data <- jsonlite::fromJSON(countryocean_source)$codes
+# Process the data
+countryocean_source <- countryocean_data %>%
+  dplyr::select(-providers) %>%
+  dplyr::arrange(desc)
+# Create choices for UI
+countryocean_choices <- setNames(countryocean_source$value, countryocean_source$desc)
 
-# # Last run by CAM on 09/16/24
-# county = readr::read_tsv(url("https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt"), col_names = FALSE)
-# county = county%>%tidyr::separate(X1,into = c("STUSAB","STATE","COUNTY","COUNTY_NAME","COUNTY_ID"), sep=",")
-# orgs = unique(utils::read.csv(url("https://cdx.epa.gov/wqx/download/DomainValues/Organization.CSV"))$ID)
-# chars = unique(utils::read.csv(url("https://cdx.epa.gov/wqx/download/DomainValues/Characteristic.CSV"))$Name)
-# chargroup = unique(utils::read.csv(url("https://cdx.epa.gov/wqx/download/DomainValues/CharacteristicGroup.CSV"))$Name)
-# media = c(unique(utils::read.csv(url("https://cdx.epa.gov/wqx/download/DomainValues/ActivityMedia.CSV"))$Name),"water","Biological Tissue","No media")
-# # sitetype = unique(utils::read.csv(url("https://cdx.epa.gov/wqx/download/DomainValues/MonitoringLocationType.CSV"))$Name)
-# sitetype = c("Aggregate groundwater use","Aggregate surface-water-use","Aggregate water-use establishment","Atmosphere","Estuary","Facility","Glacier","Lake, Reservoir, Impoundment","Land","Not Assigned","Ocean","Spring","Stream","Subsurface","Well","Wetland")
-# projects = unique(data.table::fread("https://www.waterqualitydata.us/data/Project/search?mimeType=csv&zip=no&providers=NWIS&providers=STORET")$ProjectIdentifier)
-# mlids = unique(data.table::fread("https://www.waterqualitydata.us/data/Station/search?mimeType=csv&zip=no")$MonitoringLocationIdentifier)
-# save(orgs, chars, chargroup, media, county, sitetype, projects, mlids2, file = "inst/extdata/query_choices.Rdata")
+# County, orgs, chars, chargroup, media, sitetype, and projects WQP Query options
+county <- utils::read.csv(
+  file = "https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt",
+  header = FALSE,  # Specify that the file does not have a header row
+  col.names = c("STUSAB", "STATE", "COUNTY", "COUNTY_NAME", "COUNTY_ID")  # Set column names
+)
+# Beware that some of the counties are historic, see: https://github.com/DOI-USGS/dataRetrieval/issues/711
+# Using USGS counties from dataRetrieval does not resolve https://github.com/USEPA/TADAShiny/issues/231
+# county <- dataRetrieval::check_waterdata_sample_params("counties")
+orgs = unique(utils::read.csv(url(
+  "https://cdx.epa.gov/wqx/download/DomainValues/Organization.CSV"))$ID)
+chars = unique(utils::read.csv(url(
+  "https://cdx.epa.gov/wqx/download/DomainValues/Characteristic.CSV"))$Name)
+chargroup = unique(utils::read.csv(url(
+  "https://cdx.epa.gov/wqx/download/DomainValues/CharacteristicGroup.CSV"))$Name)
+media = c(unique(utils::read.csv(url(
+  "https://cdx.epa.gov/wqx/download/DomainValues/ActivityMedia.CSV"))$Name),
+  "water","Biological Tissue","No media")
+sitetype = c(unique(utils::read.csv(url(
+  "https://cdx.epa.gov/wqx/download/DomainValues/MonitoringLocationType.CSV"))$Name), 
+  "Glacier", "Aggregate water-use establishment", "Not Assigned", "Subsurface")
+# Get projects
+# Define the URL
+project_url <- "https://www.waterqualitydata.us/data/Project/search?mimeType=csv&zip=no&providers=NWIS&providers=STORET"
+# Fetch and read CSV data using httr
+response <- httr::GET(project_url)
+httr::stop_for_status(response)  # Check for request errors
+project_data <- httr::content(response, as = "text", encoding = "UTF-8")
+# Read and process the data with fread
+projects <- data.table::fread(project_data)$ProjectIdentifier
+unique_projects <- unique(projects)
 
 mod_query_data_ui <- function(id) {
   ns <- NS(id)
@@ -535,7 +564,7 @@ mod_query_data_server <- function(id, tadat) {
     shiny::updateSelectizeInput(
       session,
       "siteid",
-      choices = c(mlids2),
+      choices = c(mlids),
       options = list(placeholder = "Start typing or use drop down menu"),
       server = TRUE
     )
