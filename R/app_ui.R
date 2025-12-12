f7ac31b379d0f89dd43c6bac088eebba86526e1a
James Bisese <james.bisese@tetratech.com>
2025-12-12 09:06:46 -0700

    fix reload error on disabled UI tabs
    Added CSS directive to prevent clicks on disabled UI tabs from reloading application.  Issue #237

--- R/app_ui.R
+++ R/app_ui.R
@@ -11,10 +11,14 @@ css <- "
 .nav li a.disabled {
   background-color: #F5F5F5 !important;
   color: #333 !important;
+  pointer-events: none; /* added 2025-Nov-17 Disable mouse events */
   cursor: not-allowed !important;
   border-color: #F5F5F5 !important;
 }
-
+/* This displays the custom cursor on the disabled anchor.  It gets overwritten when the nav li is activated. */
+.nav li {
+  cursor: not-allowed !important;
+}
 .row {
     margin-right: 0px;
     margin-left: 0px;
