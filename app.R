c9f145b40d5f712fca60bc8f0e0acd557fbb1f0f
Mullin <Mullin.Cristina@epa.gov>
2025-12-29 09:33:17 -0500

    update app.R and deploy

--- app.R
+++ app.R
@@ -1,6 +1,5 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
+# Launch the ShinyApp (Do not remove this file)
+# To deploy, run the script in 04_deploy.R
 
 # Suppress Shiny support autoloading (extra safety; not strictly necessary if inst/app has no R/)
 options(shiny.autoload.r = FALSE)
@@ -10,11 +9,5 @@ pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 # Golem production mode
 options(golem.app.prod = TRUE)
 
-# Explicitly load your package (helps dependency detection)
-if (!requireNamespace("TADAShiny", quietly = TRUE)) {
-  stop("Package 'TADAShiny' must be installable on the server.")
-}
-library(TADAShiny)
-
 # Start the golem app
-TADAShiny::run_app() # add parameters here (if any)
+run_app()
