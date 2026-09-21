6bd7ac5bcb101b02f25072ea9fec9981fdd838a0
Mullin <Mullin.Cristina@epa.gov>
2026-09-21 14:00:46 -0400

    deploy

--- dev/04_deploy.R
+++ dev/04_deploy.R
@@ -35,8 +35,11 @@ golem::document_and_reload()
 # # Use packrat
 # # options(rsconnect.packrat = TRUE) # already done
 
+# remove before deployment
+remove.packages("TADAShiny")
+
 # Deploy app to staging
-# https://rstudio-connect.dmap-stage.aws.epa.gov/content/08e5c894-ece9-4e4e-9e55-0683bef17a65/
+# https://rstudio-connect.dmap-stage.aws.epa.gov/content/963edc7d-77af-4e4d-ae7d-75d04ff6430e/
 # install.packages("rsconnect") # update to latest
 # do not edit below
 rsconnect::deployApp(
@@ -46,7 +49,7 @@ rsconnect::deployApp(
   appTitle = "TADAShiny Module 1 WQP Data Discovery and Cleaning",
   launch.browser = TRUE,
   forceUpdate = TRUE
-  # appId = 1652 8/17/26
+  # appId = 1724 9/21/26
 )
 
 # To deploy to EPA posit connect production (public)
